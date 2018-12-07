2cb9de573e831d67bc41d705e9f2010bfa5def35
George Hart <george.titta@gmail.com>
2018-12-07 19:26:02 +0100

    ESP32_Analog In test

---
+++ esp32A0.ino.ino
@@ -0,0 +1,67 @@
+/*******************************************************
+TFE 2019
+ESP32
+
+Calibration \checker based on a MAXIM 4466 Amplifier
+
+Hart Georges
+
+********************************************************/
+
+int sensorPin = A0;
+unsigned int sample;
+const int sampleWindow = 50;
+
+ 
+void setup()
+{
+   Serial.begin(115200);
+}
+ 
+ 
+void loop()
+{
+
+// ------------------------------------------------------------------
+
+   unsigned long startMillis= millis();
+   unsigned int peakToPeak = 0;
+   unsigned int signalMax = 0;
+   unsigned int signalMin = 1024;
+ 
+   
+   while (millis() - startMillis < sampleWindow)
+   {
+    sample = analogRead(sensorPin);
+      if (sample < 1024)
+      {  
+        if (sample > signalMax)
+        {
+          signalMax = sample;
+        }
+        else if (sample < signalMin)
+        {
+            signalMin = sample;
+        }
+      }
+   }
+   peakToPeak = signalMax - signalMin;
+   double volts = ((peakToPeak * 15) / 1024);
+   double rms = (volts*(sqrt(2)/2));
+
+/* ------------------------------------------   
+      Serial.print(100* volts);
+      Serial.print(" - ");
+--------------------------------------------- */
+      Serial.println(rms*100);
+}
+
+
+
+
+
+
+
+
+  
+
